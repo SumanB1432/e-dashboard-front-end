d76b24f5eda08cf435e959f288c47d3fee63683c
Suman Bera <suman80bera@gmail.com>
2023-02-17 02:08:00 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import Nav from './Nav';
-import { BrowserRouter } from 'react-router-dom';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <div >
       <BrowserRouter>
       <Nav />
+      <Routes>
+        <Route path="/" element={<h1>product listing component</h1>}></Route>
+        <Route path="/add" element={<h1>Add product component</h1>}></Route>
+        <Route path="/update" element={<h1>product update</h1>}></Route>
+        <Route path="/logout" element={<h1>logout component</h1>}></Route>
+        <Route path="/profile" element={<h1>profile component</h1>}></Route>
+      </Routes>
       </BrowserRouter>
+      <Footer/>
       
     </div>
   )
---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "../App.css"
+
+function Nav() {
+  return (
+    <div>
+        <ul className='nav-ui'>
+            <li><Link to="/">Products</Link></li>
+            <li><Link to="/add">Add Product</Link></li>
+            <li> <Link to="/update">Update Product</Link></li>
+            <li> <Link to="/logout">Logout</Link></li>
+            <li> <Link to="/profile">Profile</Link></li>
+
+               
+            
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
